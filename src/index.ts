362309969ae28d149d74a305d1298feef98a88c1
migue <mmoyano16@gmail.com>
2024-08-12 20:54:40 -0300

    ddsadsa

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,44 @@
-// import { engineInit } from "littlejs.esm"
-import { mainCanvasSize, drawText, setCameraPos, engineInit, EngineObject, vec2, randColor } from "../node_modules/littlejsengine/dist/littlejs.esm"
+// // import { engineInit } from "littlejs.esm"
+// import { Color, drawRect, setCanvasFixedSize, mainCanvasSize, drawTextScreen, setCameraPos, engineInit, EngineObject, vec2, randColor } from "../node_modules/littlejsengine/dist/littlejs.esm"
 
-function gameInit() {
+// const levelSize = vec2(38, 20);
+// const cameraPos = levelSize.scale(.5);
 
-  const levelSize = vec2(20, 20);
-  for (let x = 0; x <= levelSize.x; x += 2)
-    for (let y = 0; y <= levelSize.y; y += 1) {
-      const brick = new EngineObject(vec2(x, y), vec2(2, 1));
-      brick.color = randColor(); // give brick a random color
-    }
+// function gameInit() {
+//   setCanvasFixedSize(vec2(1280, 720));
 
-  setCameraPos(levelSize.scale(.5))
-  drawText("Hola pa", mainCanvasSize.scale(.5), 80);
-}
+//   // for (let x = 0; x <= levelSize.x; x += 2)
+//   //   for (let y = 0; y <= levelSize.y; y += 1) {
+//   //     const brick = new EngineObject(vec2(x, y), vec2(2, 1));
+//   //     brick.color = randColor(); // give brick a random color
+//   //   }
 
-function gameUpdate() {
 
-}
 
-function gameUpdatePost() {
+//   setCameraPos(cameraPos)
+// }
 
-}
+// function gameUpdate() {
 
-function gameRender() {
+// }
 
-}
+// function gameUpdatePost() {
 
-function gameRenderPost() {
+// }
 
-}
+// function gameRender() {
+//   drawRect(cameraPos, vec2(100), new Color(.5, .5, .5)); // draw background
+//   drawRect(cameraPos, levelSize, new Color(.1, .1, .1)); // draw level boundary
 
-engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost);
+
+//   drawRect(vec2(0, 0), vec2(2, 1), new Color(255, 0, 0));
+// }
+
+// function gameRenderPost() {
+//   drawTextScreen("Hola pa", mainCanvasSize.scale(.5), 80);
+
+// }
+
+// engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost);
 
 
